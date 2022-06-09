7a07b98de0736460ee3b6aec2a35b3a55d7563d4
chelizichen <1347290221@qq.com>
2022-06-09 17:10:44 +0800

    Initial Commit

---
+++ cloud/cloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudApp.swift
+//  cloud
+//
+//  Created by 彭城 on 2022/6/9.
+//
+
+import SwiftUI
+
+@main
+struct cloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
